0955df5074c01fdf169b39e6e49622c83b41e9d4
Xander Hill <xander.k.hill@gmail.com>
2024-11-30 15:25:46 -0600

    Middleware and logger added

--- app.js
+++ app.js
@@ -2,16 +2,30 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
+logger.info('connecting to', config.MONGO_URI)
+
 mongoose.connect(config.MONGO_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connnecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
