47dd9d53ce1a9d52ce25dfc108a6c8ccdfeaae96
Xander Hill <xander.k.hill@gmail.com>
2024-12-01 22:43:41 -0600

    functionality for deleting a blog, test for this

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -95,6 +95,23 @@ test('posting missing title and url property result in bad request', async () =>
     assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 })
 
+test('deleting a blog is possible', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    assert(!titles.includes('BlogBois: we out here'))
+
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
