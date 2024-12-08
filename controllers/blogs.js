4c9ecfb117fb190544f05a7b3fe5e1da7d387940
Xander Hill <xander.k.hill@gmail.com>
2024-12-07 18:08:11 -0600

    blogs display users and users display blog data

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,21 +3,30 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user')
     response.json(blogs)
 })
   
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const users = await User.find({})
+    const user = users[0]
+    console.log(users)
+
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
+      user: user,
     })
 
     const savedBlog = await blog.save()
+    console.log(savedBlog)
+    user.blogs = user.blogs.concat(savedBlog)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
