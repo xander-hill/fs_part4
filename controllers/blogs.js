495c3ab56837924248ad873c3c1abc6df17fbbc6
Xander Hill <xander.k.hill@gmail.com>
2024-12-07 18:17:44 -0600

    get request or specific id

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,6 +30,15 @@ blogsRouter.post('/', async (request, response) => {
     response.status(201).json(savedBlog)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
 blogsRouter.delete('/:id', async (request,response) => {
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
