6ceb87e066f8af7b2ee0510c230c76cecadddeca
Xander Hill <xander.k.hill@gmail.com>
2024-12-07 18:53:37 -0600

    user token and user token authentication added: may need to change if want to store complete user objects within blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,25 +1,36 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user')
     response.json(blogs)
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
   
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if(!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
 
-    const users = await User.find({})
-    const user = users[0]
-    console.log(users)
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
-      user: user,
+      user: user._id,
     })
 
     const savedBlog = await blog.save()
